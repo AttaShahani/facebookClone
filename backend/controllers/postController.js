171c820947afff0e4e12e5b106bf0b0b0ca0df5b
AttaShahani <shahaniattamuhammad786@gmail.com>
2022-04-05 12:44:11 +0500

    Post update delete and like unlike routes added

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -23,3 +23,66 @@ exports.createPost = asyncErrors(async (req,res,next)=>{
         newPost,
     })
 })
+// Update Post 
+exports.updatePost = asyncErrors(async (req,res,next)=>{
+    const post = await Post.findById(req.params.id);
+    const updatePostData = {
+        caption: req.body.caption
+    }
+    if(!post){
+        return next(new ErrorHandler("Post Not Found",404))
+    }
+    if(post.owner.toString() !== req.user.id.toString()){
+        return next(new ErrorHandler("You are not allowed to update this post",401))
+    }
+
+    const updatedPost = await post.updateOne(updatePostData,{new:true})
+    res.status(200).json({
+        success: true,
+        message: "Post Updated Successfully",
+        updatedPost
+    })
+
+})
+// Delete Post 
+exports.deletePost = asyncErrors(async (req,res,next)=>{
+    const post = await Post.findById(req.params.id);
+    const postOwner = await User.findById(req.user.id)
+    if(!post){
+        return next(new ErrorHandler("Post Not Found",404))
+    }
+    if(post.owner.toString() !== req.user.id.toString()){
+        return next(new ErrorHandler("You are not allowed to delete this post",401))
+    }
+
+    if(postOwner.posts.includes(post._id)){
+        await postOwner.updateOne({$pull:{posts: post._id}})
+    }
+    await post.remove()
+
+    res.status(200).json({
+        success : true,
+        message: "Post remove Successfully"
+    })
+})
+
+// Like and Unlike Post 
+exports.likeUnlikePost = asyncErrors(async (req,res,next)=>{
+    const post = await Post.findById(req.params.id);
+
+    if(!post){
+        return next(new ErrorHandler("Post Not Found",404))
+    }
+    if(post.likes.includes(req.user.id)){
+        await post.updateOne({$pull:{likes:req.user.id}})
+        return res.status(200).json({
+            success: true,
+            message: "Post Unliked Successfully"
+        })
+    }
+    await post.updateOne({$push:{likes:req.user.id}})
+    res.status(200).json({
+        success: true,
+        message: "Post Liked Successfully"
+    })
+})
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,8 +1,17 @@
-const router = require("express").Router()
-const { createPost } = require("../controllers/postController");
-const {isAuthenticatedUser} = require("../middlewares/auth")
+const router = require("express").Router();
+const {
+  createPost,
+  updatePost,
+  deletePost,
+  likeUnlikePost,
+} = require("../controllers/postController");
+const { isAuthenticatedUser } = require("../middlewares/auth");
 
-
-router.route("/post/new").post(isAuthenticatedUser,createPost)
+router.route("/post/new").post(isAuthenticatedUser, createPost);
+router
+  .route("/post/:id")
+  .put(isAuthenticatedUser, updatePost)
+  .delete(isAuthenticatedUser, deletePost)
+  .post(isAuthenticatedUser, likeUnlikePost);
 
 module.exports = router;
